8a5d1b7ef4e5bb1c11e12530d1790b3306ddf97e
gabz2407 <162704010+gabz2407@users.noreply.github.com>
2024-03-11 22:47:35 +0000

    Refactor JS, update cities

--- script/script.js
+++ script/script.js
@@ -1,41 +1,51 @@
 function updateCityTZ(selector, timezone) {
   let element = document.querySelector(selector);
-  let dateElement = element.querySelector(".date");
-  let timeElement = element.querySelector(".time");
-  dateElement.innerHTML = moment().tz(timezone).format("MMMM, Do");
+  if (element) {
+    let dateElement = element.querySelector(".date");
+    let timeElement = element.querySelector(".time");
+    dateElement.innerHTML = moment().tz(timezone).format("MMMM, Do");
 
-  let hours = moment().tz(timezone).format("h[h]mm:ss");
-  let am = moment().tz(timezone).format("A");
-  timeElement.innerHTML = `${hours} <span>${am}</span>`;
+    let hours = moment().tz(timezone).format("h[h]mm:ss");
+    let am = moment().tz(timezone).format("A");
+    timeElement.innerHTML = `${hours} <span>${am}</span>`;
+  }
 }
 
 function updateContent() {
-  updateCityTZ("#london", "Europe/London");
+  updateCityTZ("#singapore", "Asia/Singapore");
   updateCityTZ("#lisbon", "Europe/Lisbon");
 }
 
 function changeCity(event) {
   let citySelected = event.target.value;
-  if (citySelected === "current") {
-    citySelected = moment.tz.guess();
-  }
-  if (event.target.value.length > 0) {
-    let date = moment().tz(`${event.target.value}`).format("Do, MMMM");
+  if (citySelected) {
+    if (citySelected === "current") {
+      citySelected = moment.tz.guess();
+    }
+
+    let date = moment().tz(`${citySelected}`).format("Do, MMMM");
+
     let time = moment()
-      .tz(`${event.target.value}`)
+      .tz(`${citySelected}`)
       .format("h[h]mm:ss [<span>] A[</span>]");
 
     let cityName = citySelected.replace("_", " ").split("/")[1];
 
     let cityTimezone = document.querySelector("#data");
     cityTimezone.innerHTML = `
-      <div class="cities">
+      <div class="cities" id="selected-element">
         <div>
           <h2 class="city-selected">${cityName}</h2>
           <div class="date">${date}</div>
         </div>
         <div class="time">${time}</div>
       </div>`;
+
+    clearInterval(intervalId);
+
+    intervalId = setInterval(function () {
+      updateCityTZ("#selected-element", citySelected);
+    }, 1000);
   }
 }
 
@@ -43,3 +53,4 @@ let selectListElement = document.querySelector("#select-city");
 selectListElement.addEventListener("change", changeCity);
 
 updateContent();
+let intervalId = setInterval(updateContent, 1000);
